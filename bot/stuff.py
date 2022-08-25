2f5068b670462226926772004e84878cdaae9530
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-08-25 12:38:04 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -23,18 +23,18 @@ async def up(event):
     ed = dt.now()
     v = ts(int((ed - uptime).seconds) * 1000)
     ms = (ed - stt).microseconds / 1000
-    p = f"🌋Pɪɴɢ = {ms}ms"
+    p = f"Pɪɴɢ  = {ms}ms"
     await event.reply(v + "\n" + p)
 
 
 async def start(event):
     await event.reply(
-        f"Hi `{event.sender.first_name}`\nDo I Need To Say Something?",
+        f"Hi `{event.sender.first_name}`\nYou are a Sudo User\n You can use this bot. Start By Sending Files",
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("Modified", url="https://t.me/tomanguy"),
-                Button.url("DEVELOPER", url="t.me/tomanguy"),
+                Button.url("Source/Repo", url=f"https://giithub/Yuu-456/video_encoder_Bot"),
+                Button.url("Owner", url=f"t.me/{OWNER_USERNAME}}"),
             ],
         ],
     )
@@ -42,25 +42,23 @@ async def start(event):
 
 async def help(event):
     await event.reply(
-        "This Bot Is Not Public**\n\n✔️Only Authenticated Users Will Use This Bot"
+        "This Bot Is Not Public**\n\nOnly Authenticated Users Will Use This Bot"
     )
 
 
 async def ihelp(event):
     await event.edit(
-        "This Bot Is Not Public**\n\n✔️Only Authenticated Users Will Use This Bot",
+        "This Bot Is Not Public**\n\nOnly Authenticated Users Will Use This Bot",
         buttons=[Button.inline("BACK", data="beck")],
     )
-
-
 async def beck(event):
     await event.edit(
-        f"Hi `{event.sender.first_name}`\nThis Bot Is Not Public**\n\n✔️Only Authenticated Users Will Use This Bot",
+        f"Hi `{event.sender.first_name}`\nThis Bot Is Not Public**\n\nOnly Authenticated Users Will Use This Bot.\n\nYou Can Create Your Own Bot From The Bot's Source Codes Which Will Work The Same Way.  \n\n   ",
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("Modified", url="https://t.me/tomanguy"),
-                Button.url("DEVELOPER", url="t.me/tomanguy"),
+                Button.url("Owner", url=f"https://t.me/{OWNER_USERNAME}"),
+                Button.url("Source Code", url=f"github.com/Yuu-456/video_encoder_bot"),
             ],
         ],
     )
